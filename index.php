0b9c317150f3ebc7aa65c80354a2dfdf5b4514ac
Ania pq <melanpq@gmail.com>
2023-06-08 21:01:28 -0500

    configuracion del entorno

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="public/css/login.css">
+    <title>Login de usuario</title>
+  </head>
+  <body>
+    
+  
+    
+  </body>
+</html>
