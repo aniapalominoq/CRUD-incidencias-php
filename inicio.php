8a53a8f6f58023cb41393391c25051e1100992c2
Ania pq <melanpq@gmail.com>
2023-06-09 13:26:34 -0500

    maquetacion del la secion inicio

---
+++ inicio.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="public/css/inicio.css">
+    <script src="https://kit.fontawesome.com/83cb85df21.js" crossorigin="anonymous"></script>
+    <title>inicio</title>
+  </head>
+  <body class="body">
+    <section class="inicio__menu-collapsed">
+        <!-- HEADER -->
+        <section class="inicio__header">
+            <h1 class="inicio__header-title">
+                <span class="inicio__header-span"></span>
+            </h1>
+            <div class="inicio__header-btn">
+                <div class="inicio__header-btn-hamburger"></div>
+                <div class="inicio__header-btn-hamburger"></div>
+                <div class="inicio__header-btn-hamburger"></div>
+            </div>
+        </section>
+        <!-- PROFILE -->
+        <section class="inicio__profile" id="profile">
+            <div class="inicio__profile-photo" id="photo">
+                <img src="./public/img/Hakon-Wium.jpg" alt="foto del usuario">
+            </div>
+            <div  class="inicio__profile-name" id="name">
+                <span class="inicio__profile-span">melania palomino</span>
+            </div>
+        </section>
+
+        <!-- ITEM -->
+        <section class="inicio__items"id="menu-items">
+          
+                <a href="" class="inicio__item" >
+                    <i class="fa-solid fa-square-plus"></i>
+                    <div class="title">INCIDENCIAS</div>
+                </a>
+                  <a href="" class="inicio__item">
+                  <i class="fa-solid fa-table"></i>
+                    <div class="title">LISTA INCIDENCIAS</div>
+                </a>
+                 <a href="" class="inicio__item">
+                    <i class="fa-solid fa-file-arrow-down"></i>
+                    <div class="title">DESCARGA</div>
+                </a>
+          
+        </section>
+        <section class="inicio__main-container">
+            bienvenidos
+        </section>
+
+
+    </section>
+
+  </body>
+</html>
