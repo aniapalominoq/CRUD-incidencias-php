4742ddb097e68fe6f32629d4407c041303e2f53f
Ania pq <melanpq@gmail.com>
2023-06-12 11:18:54 -0500

    agregar cdn de sweetalert

--- registro.php
+++ registro.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="public/css/registro.css">
     <script src="https://kit.fontawesome.com/83cb85df21.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
     <title>Registro</title>
   </head>
   <body class="body">
@@ -19,18 +20,18 @@
         <form action="./servidor/registro/registrar.php" method="POST"class="resgistro__form">
             <div class="resgistro__div">
                 <label for="name" class="resgistro__label">Nombre usuario</label>
-                <input type="text" class="resgistro__input" name="name" placeholder="Nombre usuario">
+                <input type="text" class="resgistro__input" name="name" placeholder="Nombre usuario" required >
             </div>
             <div class="resgistro__div">
                 <label for="rol" class="resgistro__label"> Seleccione su Rol</label>
-                <select name="rol" id="rol"  class="resgistro__select">
+                <select name="rol" id="rol"  class="resgistro__select" required>
                     <option class="resgistro__select-option" value="controlador">Controlador</option>
                     <option class="resgistro__select-option" value="supervisor">Supervisor</option>
                 </select>
             </div>
              <div class="resgistro__div">
                 <label for="password1" class="resgistro__label">Contraseña</label>
-                <input type="password" class="resgistro__input" name="password1" placeholder="***********">
+                <input type="password" class="resgistro__input" name="password1" placeholder="***********" required>
             </div>
          
             <button  type="submit" class="resgistro__button">Registrarse</button>
